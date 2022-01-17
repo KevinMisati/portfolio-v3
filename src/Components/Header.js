3eef7502ee762c2cedde2db192d1ef3584f70bf5
kevinmisati <kevinmosigisi2001@gmail.com>
2022-01-17 19:32:45 +0300

    fixed the scrolling issue

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -37,7 +37,9 @@ const Header = () => {
                         </button>
                         <nav className="nav" >
                             <ul className="nav_list">
-                                <li onClick={handleNavRemoval} className="nav_item"><a href="#home" className=" link1 nav_link">Home</a></li>
+                                <li onClick={handleNavRemoval} className="nav_item">
+                                    <Link to="/" className=" link1 nav_link">Home</Link>
+                                </li>
                                 <li onClick={handleNavRemoval} className="nav_item"><a href="#services" className=" link2 nav_link">Services</a></li>
                                 <li onClick={handleNavRemoval} className="nav_item"><a href="#work" className="link4 nav_link">Projects</a></li>
 
