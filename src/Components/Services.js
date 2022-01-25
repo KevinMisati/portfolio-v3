8273e038a9478bc341565aa4de5913b32b76227b
kevinmisati <kevinmosigisi2001@gmail.com>
2022-01-25 13:30:25 +0300

    improving the UI

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Services.css"
+import coderImg from "../images/developer.svg"
 
 const Services = () => {
     return (
@@ -8,7 +9,7 @@ const Services = () => {
 <h2 class="section_title section_title_services">What I do</h2>
 <div class="underline"></div>
 <div class="services">
-    <img alt="coding" src="./images/developerActivity.svg" />
+    <img alt="coding" src={coderImg} />
 
     <div class="services_text">
         <p>I love and enjoy building cool stuff for the web. I enjoy spending my free time increasing my knowledge on Web Development and building new and
