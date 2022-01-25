2a1860e5bc5cb8eaed329e2efc73a62d7d599441
kevinmisati <kevinmosigisi2001@gmail.com>
2022-01-25 19:25:46 +0300

    AOS animations

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Intro.css"
+import { ANIMATIONTYPE } from '../Pages/Home'
 
 const Intro = () => {
     return (
 <div class="intro-container">
-<section id="home" class="intro" >
+<section data-aos={ANIMATIONTYPE} id="home" class="intro" >
 <h1 id="home" class="section_title section_title_intro">
     <span><img alt="👋" src="https://twemoji.maxcdn.com/2/72x72/1f44b.png" /></span>
     Hello , I am <strong>Kevin Misati</strong>
