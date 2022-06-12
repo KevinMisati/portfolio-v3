9585ae62e0fd6e41ad393d81ea32f294ba6c9e18
kevinmisati <kevinmosigisi2001@gmail.com>
2022-06-12 10:02:46 +0300

    added twitter link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -13,22 +13,23 @@ const Footer = () => {
 
             <ul>
                 <li class="footer_link">
-                    <a rel='' href="https://www.facebook.com/kevin.misati.37" target="_blank">
-                    <i class="fab fa-facebook fa-fw fa-2x"></i>
-                    </a>
-                </li>
-               
-                <li class="footer_link">
                     <a href="https://www.linkedin.com/in/kevin-misati-b5a399208/" target="_blank">
                     <i class="fab fa-linkedin fa-fw fa-2x">
 
                     </i>
                     </a>
-                    </li>
+                </li>
+                
                 <li class="footer_link">
                     <a href="https://github.com/KevinMisati" target="_blank">
                     <i class="fab fa-github fa-fw fa-2x"></i>
                     </a></li>
+
+                <li class="footer_link">
+                    <a rel='' href="https://twitter.com/misatikevi" target="_blank">
+                        <i class="fab fa-twitter fa-fw fa-2x"></i>
+                    </a>
+                </li>
             </ul>
             <p class="copyright">Kevin Misati &copy;2022</p>
         </footer>
